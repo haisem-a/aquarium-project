876e458b031c07d8c92c6995ddb6de1d00ba8539
haisem-a <ab.haisem@gmail.com>
2026-04-05 19:31:46 +0100

    Create tanks table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
-
+-- Tanks table schema
+CREATE TABLE tanks (
+    tank_id INTEGER PRIMARY KEY,
+    size_litres FLOAT,
+    water_type TEXT,
+    system_type TEXT,
+    location_type TEXT,
+    location TEXT
+)
