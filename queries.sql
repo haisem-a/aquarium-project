b3c31eae48c9eda32b5b95e8853738f31c91d7d4
haisem-a <ab.haisem@gmail.com>
2026-04-05 22:44:44 +0100

    Insert data to tanks table

--- queries.sql
+++ queries.sql
@@ -1,1 +1,9 @@
-
+INSERT INTO tanks
+(tank_id, size_litres, water_type, system_type, location_type, location)
+VALUES (1, 115, 'freshwater', 'semi-open', 'outdoor exhibit', '2a'),
+(2, 123, 'saltwater', 'semi-open', 'filtration room', '1c'),
+(3, 99, 'saltwater', 'open', 'outdoor exhibit', '2b'),
+(4, 95, 'saltwater', 'closed', 'storage room', '3a'),
+(5, 155, 'freshwater', 'semi-open', 'filtration room', '1b'),
+(6, 421, 'freshwater', 'open', 'outdoor exhibit', '2c'),
+(7, 216, 'saltwater', 'semi-open', 'filtration room', '1a');
